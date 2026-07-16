c0be8f9b99eccd5e93edbc0a7f3b1739ab294a64
권형도(윈컴) <wlegend04@lgupluspartners.cl.kr>
2026-07-16 16:11:47 +0900

    테스트 수정

--- src/main/java/kr/co/wincom/cicdtestapp/presentation/CicdTestController.java
+++ src/main/java/kr/co/wincom/cicdtestapp/presentation/CicdTestController.java
@@ -22,7 +22,7 @@ public class CicdTestController {
     public ResponseEntity<String> hello () {
 
         // 결과 출력
-        return ResponseEntity.ok("Hello World");
+        return ResponseEntity.ok("Hello World!!222");
     }
 
 }
