e8a3e903847fd377bfb66dfbfb1969dfeda39c20
권형도 <wlegend04@lgupluspartners.co.kr>
2026-07-27 17:19:53 +0900

    v3.0

--- src/main/java/kr/co/wincom/cicdtestapp/presentation/CicdTestController.java
+++ src/main/java/kr/co/wincom/cicdtestapp/presentation/CicdTestController.java
@@ -22,7 +22,7 @@ public class CicdTestController {
     public ResponseEntity<String> hello () {
 
         // 결과 출력
-        return ResponseEntity.ok("Hello World. Ansible & Jenkins 연동 CI/CD 성공!!(Ver2.0)");
+        return ResponseEntity.ok("Hello World. Ansible & Jenkins 연동 CI/CD 성공!!(Ver3.0)");
     }
 
 }
